70ba60a12e0788bd38f6bc941d7dbc2e5e7fbeac
Raben <rubenxioluna@gmail.com>
2025-10-10 09:09:08 +0200

    FEAT: Creacion de codigo estructural

--- src/main/java/org/equiporon/Controlador/Controlador.java
+++ src/main/java/org/equiporon/Controlador/Controlador.java
@@ -1,4 +1,29 @@
 package org.equiporon.Controlador;
 
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.time.LocalDate;
+
+/**
+ * Controlador JavaFX encargado de gestionar la interfaz de la tabla de Hogwarts.
+ *
+ *
+ * @author Rubén
+ * @version 1.0
+ */
 public class Controlador {
+
+    //TODO: AQUI HABRA QUE COLOCAR LAS PROPIEDADES FXML
+
+    @FXML
+    public void initialize() {
+    //TODO: Metodo initialize para configurar las columnas e inicializar el controlador
+    }
+
+    //TODO: Metodos para manejadores de eventos
 }
