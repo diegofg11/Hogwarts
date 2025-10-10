66ee6618b3c8f145213bf052dcbf0aaa006e3ac2
IgorESP <igor.esparza2@gmail.com>
2025-10-10 14:25:34 +0200

    FEAT: Cambios añadidos a la rama Igor

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module org.equiporon {
     requires javafx.fxml;
     requires javafx.media;
     requires java.sql;
+    requires org.slf4j;
     opens org.equiporon to javafx.fxml;
     exports org.equiporon;
 }
