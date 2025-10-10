29186065281b690222ae6c0e9f4813dd997f3592
Raben <rubenxioluna@gmail.com>
2025-10-10 10:02:37 +0200

    FEAT: Estudiante creado a falta de los metodos

--- src/main/java/org/equiporon/Modelo/Estudiante.java
+++ src/main/java/org/equiporon/Modelo/Estudiante.java
@@ -35,6 +35,8 @@ public class Estudiante {
      * @param nombre nombre de la persona.
      * @param apellidos apellido de la persona.
      * @param casa casa del alumno de Hogwarts.
+     * @param curso curso del alumno de Hogwarts.
+     * @param patronus (animal espiritual) patronus del alumno de Hogwarts.
      */
     public Estudiante(int id, String nombre, String apellidos, String casa, int curso,String patronus) {
         this.id = id;
@@ -44,6 +46,47 @@ public class Estudiante {
         this.curso = curso;
         this.patronus = patronus;
     }
+
+    //Setters
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+    public void setCasa(String casa) {
+        this.casa = casa;
+    }
+    public void setCurso(int curso) {
+        this.curso = curso;
+    }
+    public void setPatronus(String patronus) {
+        this.patronus = patronus;
+    }
+
+    //Getters
+    public Integer getId() {
+        return id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public String getApellidos() {
+        return apellidos;
+    }
+    public String getCasa() {
+        return casa;
+    }
+    public Integer getCurso() {
+        return curso;
+    }
+    public String getPatronus() {
+        return patronus;
+    }
+
 
     //TODO: Getters y Setters
 
