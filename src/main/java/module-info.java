eaaa617e5e2adb3385b3e2e45612e97b392a5096
UnaiZugaza <yuniz4451@gmail.com>
2025-10-16 10:44:38 +0200

    FEAT: Push de los dao y conexiones de Igor

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module org.equiporon {
     requires javafx.fxml;
     requires javafx.media;
     requires java.sql;
+    requires org.slf4j;
 
     opens org.equiporon to javafx.fxml;
     opens org.equiporon.Controlador to javafx.fxml; // 👈 Añade esta línea
