2fb91c35166bea4fcd85887a6169dd9c0ff8e99d
UnaiZugaza <yuniz4451@gmail.com>
2025-10-17 09:38:44 +0200

    FIX: Añadir controlador al module-info y actualizar lanzador

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,13 @@ module org.equiporon {
     requires javafx.fxml;
     requires javafx.media;
     requires java.sql;
+    requires org.slf4j;
+
+    // 👇 HABILITA REFLEXIÓN para JavaFX
+    opens org.equiporon.Controlador to javafx.fxml;
     opens org.equiporon to javafx.fxml;
+
+    // 👇 EXPORTA LOS PAQUETES si necesitas usar las clases fuera del módulo
     exports org.equiporon;
+    exports org.equiporon.Controlador;
 }
